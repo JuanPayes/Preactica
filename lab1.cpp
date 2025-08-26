1b14ab62c12c713946931adf5cb6334a635a4c4e
JuanPayes <00005923@uca.edu.sv>
2025-08-26 07:50:30 -0600

    Feature de Alex

--- lab1.cpp
+++ lab1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
     cout << "Hola desde Develop" << endl;
-    cout << "Soy la feature de Andrea" << endl;
+    cout << "Soy la feature de Alex" << endl;
     return 0;
 }
 
